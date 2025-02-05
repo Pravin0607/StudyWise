43c3a99dc3eb513ade28dd9bd85a6d21fe7c40fa
Pravin Adhav <pravinadhav4945@gmail.com>
2025-02-05 10:13:52 +0530

    feat : added button on the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div>
+    <div className="flex justify-center items-center h-screen">
       <h1>My App</h1>
+      <Button>Click me</Button>
     </div>
   );
 }
