e26e6bfe05ee1297e70226cf0b2f430370cd089b
Pravin Adhav <pravinadhav4945@gmail.com>
2025-02-05 10:15:48 +0530

    mod : changed the flex layout and font of heading

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <h1>My App</h1>
+    <div className="flex flex-col gap-2 justify-center items-center h-screen">
+      <h1 className="text-2xl font-mono">My App</h1>
       <Button>Click me</Button>
     </div>
   );
