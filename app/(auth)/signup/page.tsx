12b97e6a9f16869659284a6cb95134f15e7de052
Pravin Adhav <pravinadhav4945@gmail.com>
2025-03-28 15:01:52 +0530

    mod : added spinner when create user is clicked

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -27,8 +27,9 @@ const SignIn = () => {
   
   const { register, handleSubmit, formState: { errors }, watch } = useForm<SignupFormData>();
   const [role, setRole] = useState("")
-
+  const [isSubmitting, setIsSubmitting] = useState(false);
   const onSubmit = async (data: SignupFormData) => {
+    setIsSubmitting(true);
     try {
       console.log("Submitting Data : ",data)
       const response = await axios.post(`${base_url}/auth/register`, {
@@ -42,6 +43,8 @@ const SignIn = () => {
       }
     } catch (error: any) {
       toast.error(error.response?.data?.message || "Something went wrong!");
+    }finally{
+      setIsSubmitting(false);
     }
   };
 
@@ -231,11 +234,38 @@ const SignIn = () => {
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.5 }}
               >
-                <Button
-                  type="submit"
-                  className="w-full py-3 px-4 bg-blue-600 text-white font-medium rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-colors"
+<Button
+                type="submit"
+                className="w-full py-3 px-4 bg-blue-600 text-white"
+                disabled={isSubmitting}
                 >
-                  <Large>Create Account</Large>
+                {isSubmitting ? (
+                  <div className="flex items-center justify-center">
+                  <svg 
+                    className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" 
+                    xmlns="http://www.w3.org/2000/svg" 
+                    fill="none" 
+                    viewBox="0 0 24 24"
+                  >
+                    <circle 
+                    className="opacity-25" 
+                    cx="12" 
+                    cy="12" 
+                    r="10" 
+                    stroke="currentColor" 
+                    strokeWidth="4"
+                    ></circle>
+                    <path 
+                    className="opacity-75" 
+                    fill="currentColor" 
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                    ></path>
+                  </svg>
+                  <span>Processing...</span>
+                  </div>
+                ) : (
+                  <Large>Log In</Large>
+                )}
                 </Button>
               </motion.div>
 
